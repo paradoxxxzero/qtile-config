ec11b5204f3d64cc66abab822ed0ed2333fa2b49
Mounier Florian <paradoxxx.zero@gmail.com>
2011-11-17 08:04:58 +0100

    Small changes

--- config.py
+++ config.py
@@ -4,70 +4,26 @@ from libqtile import layout, bar, widget
 
 mod = 'mod4'
 keys = [
-    Key(
-        [mod], "k",
-        lazy.layout.down()
-    ),
-    Key(
-        [mod], "j",
-        lazy.layout.up()
-    ),
-    Key(
-        [mod], "f",
-        lazy.window.toggle_floating()
-    ),
-    Key(
-        [mod], "q",
-        lazy.restart()
-    ),
-    Key(
-        [mod], "space",
-        lazy.nextlayout()
-    ),
-    Key([mod], "Tab",
-        lazy.layout.next()
-    ),
-    Key([mod, "shift"], "Tab",
-        lazy.layout.previous()
-    ),
-    Key(
-        [mod, "shift"], "space",
-        lazy.layout.rotate()
-    ),
-    Key(
-        [mod, "shift"], "Return",
-        lazy.layout.toggle_split()
-    ),
-
-    Key([mod, "shift"], "Right",
-        lazy.layout.increase_ratio()),
-    Key([mod, "shift"], "Left",
-        lazy.layout.decrease_ratio()),
-
-    Key([mod], "w",      lazy.window.kill()),
-    Key([mod], "F2",     lazy.spawn(
+    Key([mod], "k", lazy.layout.down()),
+    Key([mod], "j", lazy.layout.up()),
+    Key([mod], "f", lazy.window.toggle_floating()),
+    Key([mod], "q", lazy.restart()),
+    Key([mod], "space", lazy.nextlayout()),
+    Key([mod], "Tab", lazy.layout.next()),
+    Key([mod, "shift"], "Tab", lazy.layout.previous()),
+    Key([mod, "shift"], "space", lazy.layout.rotate()),
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
+    Key([mod, "shift"], "Right", lazy.layout.increase_ratio()),
+    Key([mod, "shift"], "Left", lazy.layout.decrease_ratio()),
+    Key([mod], "w", lazy.window.kill()),
+    Key([mod], "F2", lazy.spawn(
         "dmenu_run -i -fn 'monofur:pixelsize=16:antialias=true'"
-        " -p 'Run' -nf '#ffffff' -nb '#202020'")
-    ),
-
-    Key(
-        [mod, "shift"], "k",
-        lazy.spawn("amixer -c 0 -q set Master 2dB+")
-    ),
-    Key(
-        [mod, "shift"], "j",
-        lazy.spawn("amixer -c 0 -q set Master 2dB-")
-    ),
-    Key(
-        [mod], "g",
-        lazy.togroup()
-    ),
-    Key(
-        [mod], "Left", lazy.group.prevgroup(),
-    ),
-    Key(
-        [mod], "Right", lazy.group.nextgroup(),
-    ),
+        " -p 'Run' -nf '#ffffff' -nb '#202020'")),
+    Key([mod, "shift"], "k", lazy.spawn("amixer -c 0 -q set Master 2dB+")),
+    Key([mod, "shift"], "j", lazy.spawn("amixer -c 0 -q set Master 2dB-")),
+    Key([mod], "g", lazy.togroup()),
+    Key([mod], "Left", lazy.group.prevgroup()),
+    Key([mod], "Right", lazy.group.nextgroup()),
 ]
 
 mouse = [
@@ -78,13 +34,8 @@ mouse = [
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
-# Next, we specify group names, and use the group name
-# list to generate an appropriate
-# set of bindings for group switching.
-groups = [
-    #    Group("1"),
-    #    Group("2"),
-]
+groups = []
+
 for i in groups:
     keys.append(
         Key(["mod4"], i.name, lazy.group[i.name].toscreen())
@@ -105,29 +56,30 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.GroupBox(borderwidth=2,
-                                fontsize=14,
-                                padding=1, margin_y=1),
+                widget.GroupBox(
+                    borderwidth=2, font='monofur', fontsize=14, padding=4,
+                    active="0066FF"),
                 widget.Sep(),
                 widget.Prompt(),
                 widget.WindowName(
-                    fontsize=14, margin_x=6),
-                widget.Sep(),
+                    font='monofur', fontsize=16, margin_x=6,
+                    foreground="0066FF"),
                 #widget.Mpd(fontsize=16),
-                widget.Sep(),
-                widget.CPUGraph(width=50, graph_color='0066FF',
-                                fill_color='001188'),
-                widget.MemoryGraph(width=50, graph_color='22FF44',
-                                   fill_color='11AA11'),
-                widget.SwapGraph(width=50, graph_color='FF2020',
-                                 fill_color='C01010'),
-                widget.Sep(),
+                widget.CPUGraph(
+                    width=150, graph_color='0066FF', fill_color='001188',
+                    border_color='000000'),
+                widget.MemoryGraph(
+                    width=150, graph_color='22FF44', fill_color='118811',
+                    border_color='000000'),
+                widget.SwapGraph(
+                    width=150, graph_color='FF2020', fill_color='881111',
+                    border_color='000000'),
                 widget.Systray(),
-                widget.Sep(),
-                widget.Clock('%H:%M %d/%m/%y',
-                             fontsize=18, padding=6),
+                widget.Clock(
+                    '%H:%M %d/%m/%y', font='monofur', fontsize=18, padding=6,
+                    foreground="0066FF"),
             ],
-                    24
+            28
         ),
     ),
 ]
@@ -146,10 +98,11 @@ def main(qtile):
                  'spawn': 'urxvt',
                  'exclusive': True},
         'emacs': {'persist': True,
-                  'spawn': 'emacs',
+                  # 'spawn': 'emacs',
                   'exclusive': True},
-        'www': {'exclusive': True,
-                  'spawn': 'chromium'},
+        'www': {'exclusive': True
+                  # 'spawn': 'chromium'
+            },
     }
 
     apps = [
